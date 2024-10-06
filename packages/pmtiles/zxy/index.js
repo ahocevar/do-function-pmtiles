ef74b349205864b4ea64ae54cd5812dbdbcf98e4
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-10-06 16:33:01 +0200

    debug info

--- packages/pmtiles/zxy/index.js
+++ packages/pmtiles/zxy/index.js
@@ -42,7 +42,7 @@ export async function main(event, context) {
         "Content-Type": "text/plain"
       },
       statusCode: 500,
-      body: err.stack
+      body: err.stack + "\n" + JSON.stringify(event, null, 2)
     }
   }
 }
